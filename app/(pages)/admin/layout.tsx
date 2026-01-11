2903ad99604d12e0a3c0e5bef47aa0d15bce6849
muscab101 <muscabmusa27@gmail.com>
2026-01-11 15:26:17 +0000

    commit4

--- app/(pages)/admin/layout.tsx
+++ app/(pages)/admin/layout.tsx
@@ -1,22 +1,22 @@
-import AdminSidebar from "../_components/AdminSidebar"
+// import AdminSidebar from "../_components/AdminSidebar"
 
 
-export default function AdminLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <div className="flex min-h-screen bg-background">
-      {/* Sidebar-ka */}
-      <AdminSidebar />
+// export default function AdminLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <div className="flex min-h-screen bg-background">
+//       {/* Sidebar-ka */}
+//       <AdminSidebar />
       
-      {/* Content-ka (Boggaga Admin-ka) */}
-      <main className="flex-1 overflow-y-auto">
-        <div className="p-8">
-          {children}
-        </div>
-      </main>
-    </div>
-  )
-}
+//       {/* Content-ka (Boggaga Admin-ka) */}
+//       <main className="flex-1 overflow-y-auto">
+//         <div className="p-8">
+//           {children}
+//         </div>
+//       </main>
+//     </div>
+//   )
+// }
